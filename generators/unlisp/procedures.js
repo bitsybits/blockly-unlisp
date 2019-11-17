d4cd30d8496db5dc85e06b58d8f3515c0d97723b
vldmkr <vladmakaryan@gmail.com>
2019-11-17 15:19:23 +0200

    procedures implemented

--- generators/unlisp/procedures.js
+++ generators/unlisp/procedures.js
@@ -18,12 +18,6 @@ Blockly.UnLisp['procedures_defreturn'] = function (block) {
   if (xfix1) {
     xfix1 = Blockly.UnLisp.prefixLines(xfix1, Blockly.UnLisp.INDENT)
   }
-  var loopTrap = ''
-  if (Blockly.UnLisp.INFINITE_LOOP_TRAP) {
-    loopTrap = Blockly.UnLisp.prefixLines(
-      Blockly.UnLisp.injectId(Blockly.UnLisp.INFINITE_LOOP_TRAP, block),
-      Blockly.UnLisp.INDENT)
-  }
   var branch = Blockly.UnLisp.statementToCode(block, 'STACK')
   var returnValue = Blockly.UnLisp.valueToCode(block, 'RETURN', Blockly.UnLisp.ORDER_NONE) || ''
   var xfix2 = ''
@@ -31,16 +25,12 @@ Blockly.UnLisp['procedures_defreturn'] = function (block) {
     // After executing the function body, revisit this block for the return.
     xfix2 = xfix1
   }
-  if (returnValue) {
-    returnValue = Blockly.UnLisp.INDENT + returnValue + '\n'
-  } else if (!branch) {
-    branch = ''
-  }
   var args = []
   for (var i = 0; i < block.arguments_.length; i++) {
     args[i] = Blockly.UnLisp.variableDB_.getName(block.arguments_[i], Blockly.Variables.NAME_TYPE)
   }
-  var body = (xfix1 + loopTrap + branch + xfix2 + returnValue) || '()'
+  var body = (xfix1 + branch + xfix2 + returnValue) || '()'
+  body = Blockly.UnLisp.cleanCode(body)
   var code = '(defun ' + funcName + ' (' + args.join(' ') + ') ' + body + ')\n'
   code = Blockly.UnLisp.scrub_(block, code)
   // Add % so as not to collide with helper functions in definitions list.
@@ -54,21 +44,20 @@ Blockly.UnLisp['procedures_defnoreturn'] = Blockly.UnLisp['procedures_defreturn'
 
 Blockly.UnLisp['procedures_callreturn'] = function (block) {
   // Call a procedure with a return value.
-  var funcName = Blockly.UnLisp.variableDB_.getName(
-    block.getFieldValue('NAME'), Blockly.Procedures.NAME_TYPE);
-  var args = [];
+  var funcName = Blockly.UnLisp.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Procedures.NAME_TYPE)
+  var args = []
   for (var i = 0; i < block.arguments_.length; i++) {
-    args[i] = Blockly.UnLisp.valueToCode(block, 'ARG' + i,
-      Blockly.UnLisp.ORDER_NONE) || 'nil';
+    args[i] = Blockly.UnLisp.valueToCode(block, 'ARG' + i, Blockly.UnLisp.ORDER_NONE) || '()'
   }
-  var code = funcName + '(' + args.join(', ') + ')';
-  return [code, Blockly.UnLisp.ORDER_HIGH];
-};
+  var argList = args.length ? ' ' + args.join(' ') : ''
+  var code = '(' + funcName + argList + ')'
+  return [code, Blockly.UnLisp.ORDER_HIGH]
+}
 
 Blockly.UnLisp['procedures_callnoreturn'] = function (block) {
   // Call a procedure with no return value.
   // Generated code is for a function call as a statement is the same as a
-  // function call as a value, with the addition of line ending.
-  var tuple = Blockly.UnLisp['procedures_callreturn'](block);
-  return tuple[0] + '\n';
+  // function call as a value
+  var tuple = Blockly.UnLisp['procedures_callreturn'](block)
+  return tuple[0]
 }
